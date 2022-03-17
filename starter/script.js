a66da9a16281f859dcc19f3a94d65cd5735736a7
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-03-16 22:16:31 -0400

    added logical operators notes

--- starter/script.js
+++ starter/script.js
@@ -37,23 +37,60 @@ const restaurant = {
     orderPasta: function(ing1, ing2, ing3) {
       console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
 
+    },
+
+    orderPizza: function(mainIngredient, ...otherIngredients) {
+      console.log(mainIngredient)
+      console.log(otherIngredients)
     }
 };
 
+// Logical Operators
+// Use any data type, return any data type, short-circuiting
+// short-circuiting - if first value is truthy, it will immediately return that first value
+console.log(3 || 'Jonas')
+console.log('' || 'Jonas')
+console.log(true || 0)
+console.log(undefined || null)
+
+console.log(undefined || 0 || "" || "hello" || 23 || null)
+
+// 1) Destructuring
+
 // Spread, because on Right side of = 
-const arr = [1, 2, ...[3, 4]]
+// const arr = [1, 2, ...[3, 4]]
 
 // Rest, because on Left side of =
-const [a, b, ...others] = [1, 2, 3, 4, 5]
-console.log(a, b, others)
+// const [a, b, ...others] = [1, 2, 3, 4, 5]
+// console.log(a, b, others)
 
-const [pizza, ,risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu]
-console.log(pizza, risotto, otherFood)
+// const [pizza, ,risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu]
+// console.log(pizza, risotto, otherFood)
 
 // Objects
-const { sat, ...weekdays } = restaurant.openingHours
-console.log(weekdays)
+// const { sat, ...weekdays } = restaurant.openingHours
+// console.log(weekdays)
  
+// 2) Functions
+// const add = function(...numbers) {
+//   let sum = 0
+//   for(let i = 0; i < numbers.length; i++) sum += numbers[i]
+//   console.log(sum)
+// }
+
+// add(2, 3)
+// add(5, 3, 7, 2)
+// add(8, 2, 5, 3, 2, 1, 4)
+
+// const x = [23, 5, 7]
+// add(...x)
+
+// restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach')
+// restaurant.orderPizza('mushrooms')
+
+// Spread Operator - used where we would otherwise write values separated by a comma.
+
+// Rest Pattern - used where we would otherwise write variable names separated by commas.
 
 // The Spread Operator
 

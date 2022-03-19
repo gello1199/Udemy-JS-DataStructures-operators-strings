d2356b22e557ed3b9a16e9b1437a5405cdb3b014
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-03-19 13:01:06 -0400

    for of loop notes

--- starter/script.js
+++ starter/script.js
@@ -52,6 +52,18 @@ const menu = [...restaurant.starterMenu, ...restaurant.mainMenu]
 for (const item of menu)
 console.log(item)
 
+// for (const [item] of menu.entries()) {
+//   console.log(`${item[0] + 1}: ${item[1]}`)
+// }
+
+// can destructure above code
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`)
+}
+
+// console.log([...menu.entries()])
+
 // The Logical Assignemt Operators
 
 // const rest1 = {

6ec9cb6e4e9e6b00c0b03967abc52bd8b5dfb327
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-03-20 22:35:54 -0400

    Looping objects: keys, values, entries

--- starter/script.js
+++ starter/script.js
@@ -78,7 +78,11 @@ console.log(openingHours)
 
 // Entire Object
 const entries = Object.entries(openingHours)
-console.log(entries)
+// console.log(entries)
+
+for(const [key, {open, close}] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`)
+}
 
 // if(restaurant.openingHours && restaurant.openingHours.mon)
 // console.log(restaurant.openingHours.mon.open)

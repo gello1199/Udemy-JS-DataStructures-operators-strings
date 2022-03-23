da7274217038b3292f2249acd664889e3dd6eb8e
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-03-22 21:32:23 -0400

    Sets notes

--- starter/script.js
+++ starter/script.js
@@ -60,6 +60,33 @@ const restaurant = {
     }
 };
 
+// Sets
+
+const ordersSet = new Set(['Pasta', 'Pizza', 'Pizza', 'Risotto', 'Pasta', 'Pizza'])
+
+console.log(ordersSet)
+console.log(new Set('Jonas'))
+
+console.log(ordersSet.size)
+console.log(ordersSet.has('Pizza'))
+console.log(ordersSet.has('Bread'))
+
+ordersSet.add('Garlic Bread')
+ordersSet.add('Garlic Bread')
+ordersSet.delete('Risotto')
+// ordersSet.clear()
+console.log(ordersSet)
+
+for(const order of ordersSet) console.log(order)
+
+// Example 
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']
+const staffUnique = [...new Set(staff)]
+console.log(staffUnique)
+console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size)
+
+console.log(new Set('jonasschmedtmann').size)
+
 // Property Names
 // const properties = Object.keys(openingHours)
 // console.log(properties)
@@ -496,19 +523,19 @@ BONUS: Create an object called 'scorers' which contains the names of the players
 GOOD LUCK 😀
 */
 
-// 1)
-for(const [i, player] of game.scored.entries()) 
-console.log(`Goal ${i + 1}: ${player} `)
+// // 1)
+// for(const [i, player] of game.scored.entries()) 
+// console.log(`Goal ${i + 1}: ${player} `)
 
-//2) 
-const odds = Object.values(game.odds)
-let average = 0;
-for (const odd of odds) average += odd
-  average /= odds.length
-  console.log(average)
+// //2) 
+// const odds = Object.values(game.odds)
+// let average = 0;
+// for (const odd of odds) average += odd
+//   average /= odds.length
+//   console.log(average)
 
-// 3
-for (const [team, odd] of Object.entries(game.odds)) {
-  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`
-  console.log(`Odd of ${teamStr} ${odd}`) 
-}
+// // 3
+// for (const [team, odd] of Object.entries(game.odds)) {
+//   const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`
+//   console.log(`Odd of ${teamStr} ${odd}`) 
+// }

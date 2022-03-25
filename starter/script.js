81b929d8eaec2bc025596aecb96d8a90952cabbe
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-03-25 15:56:30 -0400

    Maps: Iteration notes

--- starter/script.js
+++ starter/script.js
@@ -60,37 +60,77 @@ const restaurant = {
     }
 };
 
-// Maps - Data structure that we can use to map values to keys
+// Maps: Iteration
 
-const rest = new Map()
-rest.set('name', 'Classico Italiano')
-rest.set(1, 'Firenze, Italy')
-console.log(rest.set(2, 'Lisbon, Portugal'))
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🥳'],
+  [false, 'Try again'],
+])
 
-rest
-.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-.set('open', 11)
-.set('close', 23)
-.set(true, 'We are open')
-.set(false, 'We are closed')
+console.log(question)
 
-console.log(rest.get('name'))
-console.log(rest.get(true))
-console.log(rest.get(1))
+// convert object to map
+console.log(Object.entries(openingHours))
+const hoursMap = new Map(Object.entries(openingHours))
 
-const time = 21
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')))
+console.log(hoursMap)
 
-console.log(rest.has('categories'))
-rest.delete(2)
-// console.log(rest.clear())
-const arr = [1,2]
-rest.set(arr, 'Test')
-rest.set(document.querySelector('h1'), 'Heading')
-console.log(rest)
-console.log(rest.size)
+// Quiz app
+console.log(question.get('question'))
+for(const [key, value] of question) {
+  if(typeof key === 'number') 
+  console.log(`Answer ${key}: ${value}`)
+}
+// const answer = Number(prompt('Your answer'))
+const answer = 3
+console.log(answer)
 
-console.log(rest.get(arr))
+console.log(question.get(question.get('correct') === answer))
+
+// Convert map to array
+
+console.log([...question])
+// console.log(question.entries())
+console.log([...question.keys()])
+console.log([...question.values()])
+
+
+// Maps: Fundamentals - Data structure that we can use to map values to keys
+
+// const rest = new Map()
+// rest.set('name', 'Classico Italiano')
+// rest.set(1, 'Firenze, Italy')
+// console.log(rest.set(2, 'Lisbon, Portugal'))
+
+// rest
+// .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+// .set('open', 11)
+// .set('close', 23)
+// .set(true, 'We are open')
+// .set(false, 'We are closed')
+
+// console.log(rest.get('name'))
+// console.log(rest.get(true))
+// console.log(rest.get(1))
+
+// const time = 21
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')))
+
+// console.log(rest.has('categories'))
+// rest.delete(2)
+// // console.log(rest.clear())
+// const arr = [1,2]
+// rest.set(arr, 'Test')
+// rest.set(document.querySelector('h1'), 'Heading')
+// console.log(rest)
+// console.log(rest.size)
+
+// console.log(rest.get(arr))
 
 
 // Sets

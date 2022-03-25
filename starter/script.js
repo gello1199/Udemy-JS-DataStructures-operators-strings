43641a9cdc688286c82fe232f3f5408ba1a214b8
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-03-24 22:37:54 -0400

    Maps fundamentals notes

--- starter/script.js
+++ starter/script.js
@@ -60,32 +60,65 @@ const restaurant = {
     }
 };
 
+// Maps - Data structure that we can use to map values to keys
+
+const rest = new Map()
+rest.set('name', 'Classico Italiano')
+rest.set(1, 'Firenze, Italy')
+console.log(rest.set(2, 'Lisbon, Portugal'))
+
+rest
+.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+.set('open', 11)
+.set('close', 23)
+.set(true, 'We are open')
+.set(false, 'We are closed')
+
+console.log(rest.get('name'))
+console.log(rest.get(true))
+console.log(rest.get(1))
+
+const time = 21
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')))
+
+console.log(rest.has('categories'))
+rest.delete(2)
+// console.log(rest.clear())
+const arr = [1,2]
+rest.set(arr, 'Test')
+rest.set(document.querySelector('h1'), 'Heading')
+console.log(rest)
+console.log(rest.size)
+
+console.log(rest.get(arr))
+
+
 // Sets
 
-const ordersSet = new Set(['Pasta', 'Pizza', 'Pizza', 'Risotto', 'Pasta', 'Pizza'])
+// const ordersSet = new Set(['Pasta', 'Pizza', 'Pizza', 'Risotto', 'Pasta', 'Pizza'])
 
-console.log(ordersSet)
-console.log(new Set('Jonas'))
+// console.log(ordersSet)
+// console.log(new Set('Jonas'))
 
-console.log(ordersSet.size)
-console.log(ordersSet.has('Pizza'))
-console.log(ordersSet.has('Bread'))
+// console.log(ordersSet.size)
+// console.log(ordersSet.has('Pizza'))
+// console.log(ordersSet.has('Bread'))
 
-ordersSet.add('Garlic Bread')
-ordersSet.add('Garlic Bread')
-ordersSet.delete('Risotto')
-// ordersSet.clear()
-console.log(ordersSet)
+// ordersSet.add('Garlic Bread')
+// ordersSet.add('Garlic Bread')
+// ordersSet.delete('Risotto')
+// // ordersSet.clear()
+// console.log(ordersSet)
 
-for(const order of ordersSet) console.log(order)
+// for(const order of ordersSet) console.log(order)
 
-// Example 
-const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']
-const staffUnique = [...new Set(staff)]
-console.log(staffUnique)
-console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size)
+// // Example 
+// const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']
+// const staffUnique = [...new Set(staff)]
+// console.log(staffUnique)
+// console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size)
 
-console.log(new Set('jonasschmedtmann').size)
+// console.log(new Set('jonasschmedtmann').size)
 
 // Property Names
 // const properties = Object.keys(openingHours)

e03903778600d0a3e73edf5aee4e02f126dfe523
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-03-28 21:18:43 -0400

    Working with strings PT 3 notes

--- starter/script.js
+++ starter/script.js
@@ -85,6 +85,45 @@ const normalizedEmail = loginEmail.toLocaleLowerCase().trim()
 console.log(normalizedEmail)
 console.log(email === normalizedEmail)
 
+// replacing
+const priceGB = '288,97£'
+const priceUS = priceGB.replace('£', '$').replace(',', '.')
+console.log(priceUS)
+
+const announcement = 'All passengers come to boarding door 23. Boarding door 23!'
+
+console.log(announcement.replace('door', 'gate'))
+
+// replaceAll is new in JaveScript 
+console.log(announcement.replaceAll('door', 'gate'))
+
+// old way of changing globally
+console.log(announcement.replace(/door/g, 'gate'))
+
+// Booleans
+
+const plane = 'Airbus A320neo'
+console.log(plane.includes('A320'))
+console.log(plane.includes('Boeing'))
+console.log(plane.startsWith('Air'))
+
+if(plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the New Airbus family')
+}
+
+// Practice Exercise
+const checkBaggage = function(items) {
+  const baggage = items.toLocaleLowerCase()
+  if(baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are not allowed on board')
+  } else {
+    console.log('Welcome aboard!')
+  }
+}
+checkBaggage('I have a laptop, some Food and a pocket Knife')
+checkBaggage('Socks and camera')
+checkBaggage('Got some snacks and a gun for protection')
+
 // Working with Strings PART 1
 
 // const airline = 'TAP Air Portugal'
